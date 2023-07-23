8cc62b4c9cbf0c8f9be8d2469aec06b622af0a8f
Rithick30 <136732057+Rithick30@users.noreply.github.com>
2023-07-23 22:28:37 +0530

    Update ex_5_3.py

--- src/ex_5_3.py
+++ src/ex_5_3.py
@@ -16,4 +16,22 @@ if __name__ == "__main__":
     # Tests will run your command using a system call.
     # To test your program with arguments, run it from the command line
     # (see README.md for more details)
-    pass
+    parser=ArgumentParser(description='ex_5_3')
+    parser.add_argument('infile',help='input file')
+    parser.add_argument('outfile',help='output file')
+    args=parser.parse_args()
+
+    l1 = np.loadtxt(args.infile)
+
+    # TODO: remove the mean from l1
+    l1-=l1.mean()
+    y=l1.std()
+    processed=l1/y
+    # TODO: scale l1 so that it has a standard devitation
+    # of 1.
+    #
+    # SAVE the processed data to a variable called `processed`
+
+
+    # Here the program saves the processed data to the outfile
+    np.savetxt(args.outfile, processed, fmt='%.2e')
